02d813f8a75fffb0c278c90b90a705b8ab9b2dd7
RobertDulca <dpr200408@gmail.com>
2024-11-03 20:18:05 +0100

    setup of svelte as ui framework

---
+++ ui/src/RestClient.js
@@ -0,0 +1,34 @@
+var client = {
+    get: function(url) {
+        return fetch(url)
+            .then(response => handleResponse(response))
+            .then((text) => text.length ? JSON.parse(text) : {});
+    },
+    delete: function(url, body) {
+        return fetch(url, { method: 'DELETE', body: JSON.stringify(body) })
+            .then(response => handleResponse(response))
+            .then((text) => text.length ? JSON.parse(text) : {});
+    },
+    post: function(url, body, options) {
+        return fetch(url, { method: 'POST', body: JSON.stringify(body), headers: options })
+            .then(response => handleResponse(response))
+            .then((text) => text.length ? JSON.parse(text) : {});
+    },
+    put: function(url, body, options) {
+        return fetch(url, { method: 'PUT', body: JSON.stringify(body), headers: options })
+            .then(response => handleResponse(response))
+            .then((text) => text.length ? JSON.parse(text) : {});
+    },
+    patch: function(url, body, options) {
+        return fetch(url, { method: 'PATCH', body: JSON.stringify(body), headers: options })
+            .then(response => handleResponse(response))
+            .then((text) => text.length ? JSON.parse(text) : {});
+    }
+}
+
+function handleResponse(response) {
+    if (!response.ok) { throw response }
+    return response.text();
+}
+
+export default client;
