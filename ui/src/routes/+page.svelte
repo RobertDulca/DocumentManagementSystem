fb681a56e5df5f864d2eea363983737a216166b5
RobertDulca <dpr200408@gmail.com>
2024-11-06 02:19:49 +0100

    ui finished - upload page with text input and file upload - documents page with call to list of files

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import DocumentsTable from '$comp/LoadDocuments.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Svelt</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <DocumentsTable />
