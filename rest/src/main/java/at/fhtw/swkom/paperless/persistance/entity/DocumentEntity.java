a94645c232209cd99b257fca18a4d14194a3081f
JanSeidlmann <seidlmannj@gmail.com>
2024-11-13 11:30:39 +0100

    postgres, Entities & DTOs, Mapstruct and Testclass

--- rest/src/main/java/at/fhtw/swkom/paperless/persistance/entity/DocumentEntity.java
+++ rest/src/main/java/at/fhtw/swkom/paperless/persistance/entity/DocumentEntity.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode
-@Table(name = "documents")
 public class DocumentEntity {
 
     @Id
--- rest/src/main/java/at/fhtw/swkom/paperless/persistance/repository/DocumentRepository.java
+++ rest/src/main/java/at/fhtw/swkom/paperless/persistance/repository/DocumentRepository.java
@@ -2,7 +2,9 @@ package at.fhtw.swkom.paperless.persistance.repository;
 
 import at.fhtw.swkom.paperless.persistance.entity.DocumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentRepository extends JpaRepository<DocumentEntity, Integer> {
     DocumentEntity findByTitle(String title);
 }
