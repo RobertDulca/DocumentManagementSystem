220bbd09b77c05478a6273e9a2b0af741c60453b
JanSeidlmann <seidlmannj@gmail.com>
2024-12-10 20:01:50 +0100

    Implement Ocr

--- rest/src/main/java/at/fhtw/swkom/paperless/config/RabbitMQConfig.java
+++ rest/src/main/java/at/fhtw/swkom/paperless/config/RabbitMQConfig.java
@@ -1,11 +1,15 @@
 package at.fhtw.swkom.paperless.config;
 
+import at.fhtw.swkom.paperless.services.ServicesErrorHandler;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ErrorHandler;
 
 
 @Configuration
@@ -13,17 +17,16 @@ public class RabbitMQConfig {
 
     public static final String OCR_QUEUE_NAME = "OCR_QUEUE";
     public static final String RESULT_QUEUE_NAME = "RESULT_QUEUE";
-
+    public static final String DOCUMENT_STORAGE_PATH_PROPERTY_NAME = "FileStoragePath";
     public static final String ECHO_MESSAGE_COUNT_PROPERTY_NAME = "MessageCount";
 
     @Bean
-    public Queue echoInQueue() {
+    public Queue ocrInQueue() {
         return new Queue(OCR_QUEUE_NAME, false);
     }
 
     @Bean
-    public Queue echoOutQueue() { return new Queue(RESULT_QUEUE_NAME, false); }
-
+    public Queue ocrOutQueue() { return new Queue(RESULT_QUEUE_NAME, false); }
 
     @Bean
     public ConnectionFactory connectionFactory() {
@@ -40,4 +43,22 @@ public class RabbitMQConfig {
         return rabbitTemplate;
     }
 
+    // Custom error handling with Spring AMQP
+    // see <a href="https://www.baeldung.com/spring-amqp-error-handling">Error Handling with Spring AMQP</a>
+    // Necessary to avoid requeuing of messages with JSON-parse exceptions
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setErrorHandler(errorHandler());
+        return factory;
+    }
+
+    @Bean
+    public ErrorHandler errorHandler() {
+        return new ServicesErrorHandler();
+    }
 }
--- rest/src/main/java/at/fhtw/swkom/paperless/services/exception/StorageException.java
+++ rest/src/main/java/at/fhtw/swkom/paperless/services/exception/StorageException.java
@@ -4,4 +4,9 @@ public class StorageException extends RuntimeException {
     public StorageException(String message) {
         super(message);
     }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
